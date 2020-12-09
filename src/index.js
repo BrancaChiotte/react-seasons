ae661ffb81b62f2d1666f1f93dc4ab6f5a061f96
Branca Chiotte <brancachiotte@gmail.com>
2020-12-09 17:09:51 +0000

    running perfectly

--- src/index.js
+++ src/index.js
@@ -1,34 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Loader from "./Loader";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  // REFACTORING - less code than creating constructor method
+  state = { lat: null, errorMessage: "" };
+  // same but without refactoring:
+  // constructor(props) {
+  //   super(props);
 
-    this.state = { lat: null, errorMessage: "" };
+  //   this.state = { lat: null, errorMessage: "" };
+  // }
 
+  componentDidMount() {
+    // REFACTORING - less lines of code
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // we called setstate!!!!
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      // we called setstate!!!!
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+
+      // same but without refactoring
+      // (position) => {
+      //     this.setState({ lat: position.coords.latitude });
+      //   },
+      //   (err) => {
+      //     this.setState({ errorMessage: err.message });
+      //   }
     );
   }
 
-  // React says we have to define render!!
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading!</div>;
+    return <Loader message="Please accept location request" />;
+  }
+
+  // React says we have to define render!!
+  render() {
+    return <div className="border white">{this.renderContent()}</div>;
   }
 }
 
